6a3d6cb5341f98d71891576190b753a7da4dad17
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2025-09-30 11:51:11 +0200

    Fix API route definitions to avoid double prefix

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Http\Controllers\API\AuthTokenController;
 use App\Http\Controllers\API\StatsController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api')
-    ->middleware(['api', 'auth:sanctum'])
-    ->name('api.')
-    ->group(function () {
+Route::name('api.')->group(function () {
+    Route::post('auth/token', [AuthTokenController::class, 'store'])->name('auth.token.store');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::delete('auth/token', [AuthTokenController::class, 'destroy'])->name('auth.token.destroy');
+
         Route::get('stats', [StatsController::class, 'index'])->name('stats');
         Route::get('games/{game}/rating', [StatsController::class, 'gameRating'])->name('games.rating');
     });
+});
