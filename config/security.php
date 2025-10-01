27884b3ba4ad7d7e0e583784e47d7fa6b294e45e
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2025-10-01 10:40:57 +0200

    Refine CSP development allowances

--- config/security.php
+++ config/security.php
@@ -12,38 +12,70 @@ return [
 
     'referrer_policy' => 'strict-origin-when-cross-origin',
 
-    'csp' => [
-        'default-src' => [
-            "'self'",
-        ],
-        'script-src' => [
+    'csp' => (function () {
+        $devHostnames = [
+            'localhost',
+            '127.0.0.1',
+            '[::1]',
+        ];
+
+        $devPorts = [5173];
+
+        $devSchemes = ['http', 'https'];
+
+        $devScriptSources = [];
+
+        foreach ($devHostnames as $host) {
+            foreach ($devPorts as $port) {
+                foreach ($devSchemes as $scheme) {
+                    $devScriptSources[] = sprintf('%s://%s:%d', $scheme, $host, $port);
+                }
+            }
+        }
+
+        $devConnectSources = $devScriptSources;
+
+        foreach ($devScriptSources as $source) {
+            $devConnectSources[] = str_starts_with($source, 'https://')
+                ? preg_replace('/^https/', 'wss', $source, 1)
+                : preg_replace('/^http/', 'ws', $source, 1);
+        }
+
+        $scriptSources = array_merge([
             "'self'",
             "'unsafe-inline'",
             'https://js.stripe.com',
             'https://*.stripe.com',
             'https://*.deepl.com',
             'https://*.deeplapi.com',
-            'http://localhost:5173',
-            'https://localhost:5173',
-            'http://[::1]:5173',
-            'https://[::1]:5173',
-        ],
-        'style-src' => [
+        ], $devScriptSources);
+
+        $styleSources = array_merge([
             "'self'",
             "'unsafe-inline'",
-            'http://localhost:5173',
-            'https://localhost:5173',
             'https://fonts.bunny.net',
-        ],
-        'font-src' => [
-            "'self'",
-            'https://fonts.bunny.net',
-        ],
-        'frame-src' => [
-            "'self'",
-            'https://js.stripe.com',
-            'https://hooks.stripe.com',
-            'https://checkout.stripe.com',
-        ],
-    ],
+        ], $devScriptSources);
+
+        return [
+            'default-src' => [
+                "'self'",
+            ],
+            'script-src' => $scriptSources,
+            'script-src-elem' => $scriptSources,
+            'style-src' => $styleSources,
+            'font-src' => [
+                "'self'",
+                'https://fonts.bunny.net',
+            ],
+            'frame-src' => [
+                "'self'",
+                'https://js.stripe.com',
+                'https://hooks.stripe.com',
+                'https://checkout.stripe.com',
+            ],
+            'connect-src' => array_merge([
+                "'self'",
+            ], $devConnectSources),
+        ];
+    })(),
 ];
