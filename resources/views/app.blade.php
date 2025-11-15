1029bb9c1e2c777b06c8a01ca5b459c326d691a0
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2025-11-15 12:03:40 +0100

    Add Microsoft Clarity tracking script

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -43,6 +43,13 @@
 
         @routes
         @vite(['resources/js/app.ts'])
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, 'clarity', 'script', 'u6ggv25j95');
+        </script>
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
